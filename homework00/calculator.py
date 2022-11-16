b9ddef1379318adfc7222e12e59154d579e1f58a
sonni-a <112982739+sonni-a@users.noreply.github.com>
2022-11-16 16:39:26 +0300

    Update calculator.py

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -8,13 +8,19 @@ def kalc(a:float, command:str, b=1.0) -> tp.Union[float, str]:
     if command == 'cos':
             return cos(a)
     if command == 'tan':
+        if math.cos(num1) != 0:
             return tan(a)
+        else: return ('На 0 делить нельзя поэтому косинус не может быть равен нулю')
     if command == 'sin':
             return sin(a)
     if command == 'ln':
+        if a > 0:
             return log(a)
+        else: return 'Аргумент должен быть больше нуля'
     if command == 'lg':
+        if a > 0:
             return log(a, 10)
+        else: return 'Аргумент должен быть больше нуля'
     if command == '+':
             return a+b
     if command == '-':
@@ -30,10 +36,13 @@ def kalc(a:float, command:str, b=1.0) -> tp.Union[float, str]:
     else: return 'Вы ввели какую-то ерунду'
 
 
+print('Введите первое число:')
 a = float(input())
+print('Введите операцию')
 command = input()
 if command in ['^2', 'cos', 'tan', 'sin', 'ln', 'lg', '^']:
     print(kalc(a, command))
 elif command in ['+', '-', '*', '/']:
+    print('Введите второе число:')
     b = float(input())
     print(kalc(a, command, b))
