c1b32a6f16d54a510795896537a62769e58d60f1
sonni-a <112982739+sonni-a@users.noreply.github.com>
2022-11-16 16:16:13 +0300

    Update calculator.py

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -1,61 +1,42 @@
 import typing as tp
-from math import cos, tan, sin, log, log1p, pow
+from math import cos, tan, sin, log, pow
 
 
-def kalc(command: str) -> tp.Union[float, str]:
-    if command in ['^2', 'cos', 'tan', 'sin', 'ln', 'lg']:
-        d = float(input())
-        if command == '^2':
-            return d **2
-        if command == 'cos':
-            return cos(d)
-        if command == 'tan':
-            return tan(d)
-        if command == 'sin':
-            return sin(d)
-        if command == 'ln':
-            return log(d)
-        if command == 'lg':
-            return log(d, 10)
-    elif command in ['^', '+', '-', '/', '*']:
-        print('Введите первое число')
-        a = float(input())
-        print('Введите второе число')
-        b = float(input())
-        if command == '+':
+def kalc(a:float, command:str, b=1.0) -> tp.Union[float, str]:
+    if command == '^2':
+            return a ** 2
+    if command == 'cos':
+            return cos(a)
+    if command == 'tan':
+            return tan(a)
+    if command == 'sin':
+            return sin(a)
+    if command == 'ln':
+            return log(a)
+    if command == 'lg':
+            return log(a, 10)
+    if command == '+':
             return a+b
-        if command == '-':
+    if command == '-':
             return a-b
-        if command == '^':
+    if command == '^':
             return pow(a, b)
-        if command == '/':
+    if command == '/':
+        if b != 0:
             return a / b
-        if command == '*':
+        else: return 'На 0 делить нельзя'
+    if command == '*':
             return a*b
     else: return 'Вы ввели какую-то ерунду'
 
 
-def calc(num_1: float, num_2: float, command: str) -> tp.Union[float, str]:
-    if command == "*":
-        return num_1 * num_2
-    if command == "/":
-        return num_1 / num_2
-    if command == '**':
-        return num_1 ** num_2
-    else:
-        return f"Неизвестный оператор: {command!r}."
-
-
-def match_case_calc(num_1: float, num_2: float, command: str) -> tp.Union[float, str]:
-    match command:
-        case "*":
-            return num_1 * num_2
-        case "/":
-            return num_1 / num_2
-        case _:
-            return f"Неизвестный оператор: {command!r}."
-
-
-while True:
-    command = input('Введите операцию: ')
-    print(kalc(command))
+print('Введите первое число:')
+a = float(input())
+print('Введите операцию')
+command = input()
+if command in ['^2', 'cos', 'tan', 'sin', 'ln', 'lg', '^']:
+    print(kalc(a, command))
+elif command in ['+', '-', '*', '/']:
+    print('Введите второе число:')
+    b = float(input())
+    print(kalc(a, command, b))
